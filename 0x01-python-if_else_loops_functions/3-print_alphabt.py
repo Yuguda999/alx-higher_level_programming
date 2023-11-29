71c6aff49ab2e16f346cd01ce9bf85fa35ff8a95
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-11-29 05:38:05 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z")+1):
-    if i == ord('q') or i == ord('e'):
+    if i == ord("q") or i == ord("e"):
         continue
-    print(f'{i:c}', end="")
+
+    print("{:c}".format(i), end="")
