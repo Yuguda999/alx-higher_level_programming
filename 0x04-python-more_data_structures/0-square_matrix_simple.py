ed776b755ef04dccd0a0d14970af92e82e7f7c68
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-12-06 17:52:07 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    """
+    Square each element of matrix
+    Args:
+        matrix - a 2d lists
+    """
     if matrix is None:
         return None
     return [
